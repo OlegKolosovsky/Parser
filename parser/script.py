919b8088a77845e3bc50aa8241b91619ebe0fc23
Oleg Kolosovsky <kololerom@gmail.com>
2021-02-23 21:04:08 +0300

    first commit

--- parser/script.py
+++ parser/script.py
@@ -1,3 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+URL = 'https://www.dns-shop.ru/catalog/17a8a01d16404e77/smartfony/'
+HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36',
+           'accept': 'text/css,*/*;q=0.1'}
+
+
+def get_html(url, params=None):
+    r = requests.get(url, headers=HEADERS, params=params)
+    return r
+
+
+def parce():
+    html = get_html(URL)
+    if html.status_code == 200:
+        parce
+    else:
+        print('Не удаётся установить соединение с сайтом.')
+
+
+parce()
